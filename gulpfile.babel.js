f193ac38c687efe1d17f6de21a05fd906f42f198
James Barzegar <james.barzegar@gmail.com>
2016-12-13 21:40:09 -0500

    Inital Commit

---
+++ gulpfile.babel.js
@@ -0,0 +1,18 @@
+import gulp from 'gulp'
+
+import bs from 'browser-sync'
+
+import $ from 'gulp-util'
+
+import watcher from './tasks/watcher'
+import devServer from './conf/dev-server'
+
+gulp.task('watcher', ['dev-server'], () => {
+  watcher(bs)
+})
+
+gulp.task('dev-server', () => {
+  devServer(bs)
+})
+
+gulp.task('default', ['watcher'])
